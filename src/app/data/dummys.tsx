442be3f49bab50f2c84caa5ed0b4197e47775709
Jeffrey Sedoro <jeffreysedoro5@gmail.com>
2025-01-01 23:33:39 +0800

    idunno what i did HAHAHA

--- src/app/data/dummys.tsx
+++ src/app/data/dummys.tsx
@@ -2,5 +2,5 @@ export const DATA = {
 address: "Purok 3B Sinayawan Valencia City, Bukidnon",
 phone: "09519484871",
 email: "jeffreysedoro5@gmail.com",
-website: "https://jeffrey-sedoro.netlify.app/",
+website: "/",
 }
